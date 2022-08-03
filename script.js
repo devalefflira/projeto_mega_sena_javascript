f35d5185f43d1ac4e40de3a578e61fb3671b5df5
ALEFF JUNIO SILVA LINS LIRA <dev.alefflira@gmail.com>
2022-08-03 19:18:45 -0300

    Criação da função que adiciona o número e limitação do intervalo e da quantidade de números inseridos

--- script.js
+++ script.js
@@ -5,7 +5,29 @@ let state = {
 };
 
 function start() {
-  console.log('start');
+  addNumberToGame(1);
+  addNumberToGame(2);
+  addNumberToGame(3);
+  addNumberToGame(4);
+  addNumberToGame(5);
+  addNumberToGame(6);
+  addNumberToGame(7);
+
+  console.log(state.currentGame);
+}
+
+function addNumberToGame(numberToAdd) {
+  if (numberToAdd < 1 || numberToAdd > 60) {
+    console.error('Número inválido', numberToAdd);
+    return;
+  }
+
+  if (state.currentGame.length >= 6) {
+    console.error('O jogo já está completo.');
+    return;
+  }
+
+  state.currentGame.push(numberToAdd);
 }
 
 start();
