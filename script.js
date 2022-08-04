d1aa45dd6cd210793fea18612d0e2e40f21f91c7
ALEFF JUNIO SILVA LINS LIRA <dev.alefflira@gmail.com>
2022-08-03 22:18:55 -0300

    Criação das funções para remover, verificar duplicidade, salvar o jogo, se o jogo está completo e resetar o jogo

--- script.js
+++ script.js
@@ -11,11 +11,11 @@ function start() {
   addNumberToGame(4);
   addNumberToGame(5);
   addNumberToGame(6);
-  addNumberToGame(7);
 
   console.log(state.currentGame);
 }
 
+// ADICIONAR UM NÚMERO AO JOGO
 function addNumberToGame(numberToAdd) {
   if (numberToAdd < 1 || numberToAdd > 60) {
     console.error('Número inválido', numberToAdd);
@@ -27,7 +27,56 @@ function addNumberToGame(numberToAdd) {
     return;
   }
 
+  if (isNumberInGame(numberToAdd)) {
+    console.error('Este número já está no jogo.', numberToAdd);
+    return;
+  }
+
   state.currentGame.push(numberToAdd);
+}
+
+// REMOVER UM NÚMERO DO JOGO
+function removeNumberFromGame(numberToRemove) {
+  if (numberToRemove < 1 || numberToRemove > 60) {
+    console.error('Número inválido', numberToRemove);
+    return;
+  }
+  let newGame = [];
+  for (let i = 0; i < state.currentGame.length; i++) {
+    let currentNumber = state.currentGame[i];
+
+    if (currentNumber === numberToRemove) {
+      continue;
+    }
+
+    newGame.push(currentNumber);
+  }
+
+  state.currentGame = newGame;
+}
+
+// VERIFICAR SE JÁ EXITE O NÚMERO NO JOGO
+function isNumberInGame(numberToCheck) {
+  return state.currentGame.includes(numberToCheck);
+}
+
+// SALVAR O JOGO FEITO
+function saveGame() {
+  if (!isGameComplete()) {
+    console.error('O jogo não está completo!');
+    return;
+  }
+
+  state.savedGames.push(state.currentGame);
+}
+
+// VERIFICAR SE O JOGO ESTÁ COMPLETO
+function isGameComplete() {
+  return state.currentGame.length === 6;
+}
+
+function resetGame() {
+  state.currentGame = [];
 }
 
 start();
